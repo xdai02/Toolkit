ddb8751f27b13bfa49ebd32f9ff1064da6152aa0
xdai02 <2513276112@qq.com>
2022-08-22 15:42:10 +0800

    optimize rename files tool

--- rename_files/rename_files.py
+++ rename_files/rename_files.py
@@ -1,10 +1,16 @@
 import os
 
 path = "./"
+
 files = os.listdir(path)
 cnt = 1
+
 for file in files:
     if ".py" in file:
         continue
-    os.rename(path + file, path + str(cnt) + ".jpg")
+        
+    filename = os.path.splitext(file)[0]
+    suffix = os.path.splitext(file)[1]
+    os.rename(path + filename + suffix, path + str(cnt) + suffix)
+    
     cnt += 1
